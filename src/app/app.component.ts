959025bd191f6f1359a7ccbbb1328525c20f8fb1
ozias-bombiri <ozibombe@hotmail.com>
2026-01-23 15:57:15 +0000

    page aide

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent, NavbarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'portail-idcheck';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { VerificationComponent } from './verification/verification.component';
+import { HistoriqueComponent } from './historique/historique.component';
+import { AideComponent } from './aide/aide.component';
+import { ConnexionComponent } from './connexion/connexion.component';
+import { VerificationResultatComponent } from './verification-resultat/verification-resultat.component';
+import { authGuard } from './auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+  { path: 'verifier', component: VerificationComponent, canActivate: [authGuard] },
+  { path: 'historique', component: HistoriqueComponent, canActivate: [authGuard] },
+  { path: 'aide', component: AideComponent },
+  { path: 'connexion', component: ConnexionComponent }
+];
